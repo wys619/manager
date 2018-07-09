b74790e85e72bf7cc7b35c359a7cacdcdeec0383
wys <516060134@qq.com>
2018-07-09 18:23:05 +0800

    add next activity for register activity ..

---
+++ src/main/java/cn/woyeshi/manager/activities/RegisterNextActivity.kt
@@ -0,0 +1,27 @@
+package cn.woyeshi.manager.activities
+
+import android.os.Bundle
+import android.widget.TextView
+import cn.woyeshi.base.activities.BaseActivity
+import cn.woyeshi.manager.R
+
+class RegisterNextActivity : BaseActivity() {
+
+    override fun getContentLayoutID(): Int {
+        return R.layout.activity_register_next
+
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        setEditBtnVisibility(true)
+        setEditBtnText("跳过")
+
+    }
+
+    //点击跳过
+    override fun onEditBtnClick(view: TextView) {
+
+
+    }
+
+}
